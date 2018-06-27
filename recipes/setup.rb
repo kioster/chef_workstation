056082dffa1e0c690e754740faaca9eca442bd4c
ChefDK User <chef@ip-172-31-11-247.ec2.internal>
2018-06-27 04:50:08 +0000

    update ver 0.2.1

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -2,12 +2,6 @@ package 'tree' do
   action :install
 end
 
-file '/etc/motd' do
-  content "Property of ...
-
-  IPADDRESS: #{node['ipaddress']}
-  HOSTNAME: #{node['hostname']}
-  MEMORY: #{node['memory']['total']}
-  CPU: #{node['cpu']['0']['mhz']}
-"
+template 'etc/motd' do
+  source 'motd.erb'
 end
