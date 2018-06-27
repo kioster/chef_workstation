caf62d3f2a225f462fe2f36ac59e42edef9a9169
ChefDK User <chef@ip-172-31-11-247.ec2.internal>
2018-06-27 02:46:33 +0000

    set kitchen test

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,21 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
+describe port(81), :skip do
+  it { should_not be_listening }
+end
+# This is an example test, replace it with your own test.
+
 unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
+  describe user('root') do
     it { should exist }
   end
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
+describe port(80) do
   it { should_not be_listening }
+end
+
+describe package('tree') do
+  it { should be_installed }
 end
